e13828d0460b5da8c6fddef56c337292be735bd9
matigrzegor <matigrzegor@gmail.com>
2020-06-10 15:55:07 +0200

    Change doorkeeper config to create Doorkeeper::Application

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -17,7 +17,7 @@ Doorkeeper.configure do
   # adding oauth authorized applications. In other case it will return 403 Forbidden response
   # every time somebody will try to access the admin web interface.
   #
-  # admin_authenticator do
+  admin_authenticator do
   #   # Put your admin authentication logic here.
   #   # Example implementation:
   #
@@ -26,7 +26,7 @@ Doorkeeper.configure do
   #   else
   #     redirect_to sign_in_url
   #   end
-  # end
+  end
 
   # You can use your own model classes if you need to extend (or even override) default
   # Doorkeeper models such as `Application`, `AccessToken` and `AccessGrant.
