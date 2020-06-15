3c07d1ed36d0deabadea4e7dfa36ae4e1335102e
matigrzegor <matigrzegor@gmail.com>
2020-06-15 11:30:22 +0200

    Add grant_flows password to doorkeeper config

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -348,6 +348,8 @@ Doorkeeper.configure do
   #
   # grant_flows %w[authorization_code client_credentials]
 
+  grant_flows %w[password]
+  
   # Allows to customize OAuth grant flows that +each+ application support.
   # You can configure a custom block (or use a class respond to `#call`) that must
   # return `true` in case Application instance supports requested OAuth grant flow
