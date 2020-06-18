d144e623525ed489167be43f23a0ba55f30f55f1
matigrzegor <matigrzegor@gmail.com>
2020-06-18 09:18:39 +0200

    Change base url to prdouction url in login_check.js

--- app/javascript/packs/login_check.js
+++ app/javascript/packs/login_check.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://fierce-anchorage-12434.herokuapp.com";
 
 const renderRequestError = (errorMsg) => {
     const paragraph = document.createElement("p");
