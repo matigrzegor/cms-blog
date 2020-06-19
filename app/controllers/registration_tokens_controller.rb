7e9f68229112f954022ded0c09c8bba152cfe0a5
matigrzegor <matigrzegor@gmail.com>
2020-06-19 11:01:08 +0200

    Remove test method from registration_tokens_controller

--- app/controllers/registration_tokens_controller.rb
+++ app/controllers/registration_tokens_controller.rb
@@ -1,7 +1,6 @@
 class RegistrationTokensController < ApplicationController
     protect_from_forgery except: :create
     before_action :doorkeeper_authorize!
-    before_action :show_doorkeeper_possibilities
 
     def create
         token = create_registration_token
@@ -15,10 +14,5 @@ class RegistrationTokensController < ApplicationController
     
         def create_registration_token
             RegistrationToken.concoct
-        end
-
-        def show_doorkeeper_possibilities
-            puts doorkeeper_token
-            puts doorkeeper_token.resource_owner_id
         end
 end
