e29ad8f0ec52891996ba1ae701b53c0f724946d8
matigrzegor <matigrzegor@gmail.com>
2020-07-10 15:31:12 +0200

    Refactor paginatable

--- app/cms/paginatable.rb
+++ app/cms/paginatable.rb
@@ -1,13 +1,15 @@
 module Paginatable
 
     def page(num)
-        num = 1 if num.nil? || num == 0
-        #return nil unless num.is_a?(Integer)
+        num = num.to_i
+        num = 1 if num == 0
 
         num_on_page = 20
         num_to_fetch = num * num_on_page
         
         self.order('id desc').first(num_to_fetch)[(num_to_fetch - num_on_page)..(num_to_fetch -1)]
+    rescue
+        nil
     end
 
 end
