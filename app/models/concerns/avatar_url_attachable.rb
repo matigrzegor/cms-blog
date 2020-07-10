4f886785617a09dc17e7712e69d6a7be73b11d4f
matigrzegor <matigrzegor@gmail.com>
2020-07-10 12:15:24 +0200

    Fix avatar_url_attachable

--- app/models/concerns/avatar_url_attachable.rb
+++ app/models/concerns/avatar_url_attachable.rb
@@ -1,7 +1,12 @@
 module AvatarUrlAttachable
 
     def attach_avatar_url
-        self.avatar_url = service_url if self.avatar.attached?
+        self.avatar_url = generate_url if self.avatar.attached?
     end
     
+    private
+
+        def generate_url
+            self.avatar.service_url
+        end
 end
