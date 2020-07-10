1fb45acc55cf23d919c23fd854f3eb678390e15d
matigrzegor <matigrzegor@gmail.com>
2020-07-10 12:11:52 +0200

    CHange avatar_url_attachable url gen method

--- app/models/concerns/avatar_url_attachable.rb
+++ app/models/concerns/avatar_url_attachable.rb
@@ -1,12 +1,7 @@
 module AvatarUrlAttachable
 
     def attach_avatar_url
-        self.avatar_url = generate_avatar_url if self.avatar.attached?
+        self.avatar_url = service_url if self.avatar.attached?
     end
-
-    private 
-
-        def generate_avatar_url
-            rails_blob_path(self.avatar, disposition: "attachment", only_path: true)
-        end
+    
 end
