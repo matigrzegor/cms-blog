975f6279cac01b660d0ae896028cc9e27e7d6f62
matigrzegor <matigrzegor@gmail.com>
2020-07-17 13:25:40 +0200

    Add domain to corsd

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins ['http://localhost:8080', 'https://musing-ramanujan-8002a4.netlify.app']
     resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
   end
 end
