a6af22ceed4a71d5e492495293f24a502e7977f9
matigrzegor <matigrzegor@gmail.com>
2020-07-21 13:02:48 +0200

    Add puts statemtent to doorkeeper_headers_middleware

--- app/lib/doorkeeper_headers_middleware.rb
+++ app/lib/doorkeeper_headers_middleware.rb
@@ -7,6 +7,8 @@ class DoorkeeperHeadersMiddleware
     def call(env)
         @status, @headers, @body = @app.call(env)
 
+        put_env_hash(env)
+
         add_access_control_allow_origin_header
 
         [@status, @headers, @body]
@@ -16,8 +18,15 @@ class DoorkeeperHeadersMiddleware
 
         def add_access_control_allow_origin_header
             if @status == 302
-                @headers["Access-Control-Allow-Origin"] = 'https://musing-ramanujan-8002a4.netlify.app/admin-panel.html'
-                @headers["Access-Control-Allow-Credentials"] = 'true'
+                #@headers["Access-Control-Allow-Origin"] = 'https://musing-ramanujan-8002a4.netlify.app/admin-panel.html'
+                #@headers["Access-Control-Allow-Credentials"] = 'true'
+            end
+        end
+
+        def put_env_hash(env)
+            env.each do |k,v|
+                puts '#' * 40
+                puts "#{k}:   #{v}"
             end
         end
 end
