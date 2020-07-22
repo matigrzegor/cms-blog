461e9c9441a48e178fa936e3049e9875e63f868a
matigrzegor <matigrzegor@gmail.com>
2020-07-22 09:02:43 +0200

    Refactor avatar_url_generatable

--- app/models/concerns/avatar_url_generatable.rb
+++ app/models/concerns/avatar_url_generatable.rb
@@ -1,11 +1,14 @@
 module AvatarUrlGeneratable
 
     def generate_avatar_url
-        domain_name = ENV['DOMAIN_NAME'] || "localhost:3000"
-        
         return domain_name + rails_blob_path(self.avatar, disposition: "attachment", only_path: true) if self.avatar.attached?
-
+        
         nil
     end
     
+    private
+
+        def domain_name
+            ENV['DOMAIN_NAME'] || "localhost:3000"
+        end
 end
